77a37af4a801963b05672a52b431b9fe4a1f4353
unknown <chromegold70@gmail.com>
2021-11-03 21:25:16 -0300

    ADD Sidebar

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,4 +1,6 @@
+import { useForm } from '../../contexts/FormContext'
 import { Header } from '../Header'
+import { SidebarItem } from '../SidebarItem'
 import * as C from './styles'
 
 type Props = {
@@ -6,13 +8,39 @@ type Props = {
 }
 
 export const Theme = ({ children }: Props) => {
+  const { state } = useForm()
+
   return (
     <C.Container>
       <C.Area>
         <Header />
 
         <C.Steps>
-          <C.Sidebar>...</C.Sidebar>
+          <C.Sidebar>
+            <SidebarItem
+              title="Pessoal"
+              description="Se indentifique"
+              icon="profile"
+              path="/"
+              active={state.currentStep === 1}
+            />
+
+            <SidebarItem
+              title="Profissional"
+              description="Seu nível"
+              icon="book"
+              path="/step2"
+              active={state.currentStep === 2}
+            />
+
+            <SidebarItem
+              title="Contatos"
+              description="Como te achar"
+              icon="email"
+              path="/step3"
+              active={state.currentStep === 3}
+            />
+          </C.Sidebar>
           <C.Page>{children}</C.Page>
         </C.Steps>
       </C.Area>
